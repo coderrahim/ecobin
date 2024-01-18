db25d459fd3562168f781d76616f34e61edd2756
mrromanmiah <mrromanmiah@gmail.com>
2024-01-19 02:20:13 +0600

    banner temporarily added

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import Services from '@/components/Services/Services';
 import Footer from '@/components/Footer/Footer';
 import Navbar from '@/components/Navbar/Navbar';
 import React from 'react';
+import Banner from '@/Components/Banner/Banner';
 
 const HomePage = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Banner></Banner>
       <Services></Services>
       <Footer></Footer>
     </div>
