b6658242b3b739d33fdbfdcc9208050688b597e9
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2024-02-01 01:21:14 +0600

    Implemented TypeAnimation

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,136 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import Image from "next/image";
+
+const Navbar = () => {
+  const [isToggleOpen, setIsToggleOpen] = useState(false);
+  const path = usePathname();
+
+  return (
+    <div>
+      <header className="relative z-20 w-full bg-transparent lg:after:absolute lg:after:top-full lg:after:left-0 lg:after:z-10 lg:after:h-px lg:after:w-full lg:after:bg-slate-200 lg:backdrop-blur-sm after:hidden">
+        <div className="relative mx-auto max-w-full px-6 lg:max-w-5xl xl:max-w-7xl 2xl:max-w-[96rem]">
+          <nav
+            aria-label="main navigation"
+            className="flex h-[5.5rem] items-stretch justify-between font-medium text-slate-700"
+            role="navigation"
+          >
+            {/*      <!-- Brand logo --> */}
+            <Link
+              id="ecoBin"
+              aria-label="ecoBin logo"
+              aria-current="page"
+              className="flex items-center gap-2 whitespace-nowrap py-3 text-lg focus:outline-none lg:flex-1"
+              href={"/"}
+            >
+              <Image
+                src="/EcoBin.png"
+                width={150}
+                height={50}
+                alt="Logo"
+              ></Image>
+            </Link>
+            {/*      <!-- Mobile trigger --> */}
+            <button
+              className={`relative order-10 block h-10 w-10 self-center lg:hidden
+              ${isToggleOpen
+                  ? "visible opacity-100 [&_span:nth-child(1)]:w-6 [&_span:nth-child(1)]:translate-y-0 [&_span:nth-child(1)]:rotate-45 [&_span:nth-child(3)]:w-0 [&_span:nth-child(2)]:-rotate-45 "
+                  : ""
+                }
+            `}
+              onClick={() => setIsToggleOpen(!isToggleOpen)}
+              aria-expanded={isToggleOpen ? "true" : "false"}
+              aria-label="Toggle navigation"
+            >
+              <div className="absolute top-1/2 left-1/2 w-6 -translate-x-1/2 -translate-y-1/2 transform">
+                <span
+                  aria-hidden="true"
+                  className="absolute block h-0.5 w-9/12 -translate-y-2 transform rounded-full bg-slate-900 transition-all duration-300"
+                ></span>
+                <span
+                  aria-hidden="true"
+                  className="absolute block h-0.5 w-6 transform rounded-full bg-slate-900 transition duration-300"
+                ></span>
+                <span
+                  aria-hidden="true"
+                  className="absolute block h-0.5 w-1/2 origin-top-left translate-y-2 transform rounded-full bg-slate-900 transition-all duration-300"
+                ></span>
+              </div>
+            </button>
+            {/*      <!-- Navigation links --> */}
+            <ul
+              role="menubar"
+              aria-label="Select page"
+              className={`absolute top-0 left-0 z-[-1] h-[28.5rem] w-full justify-center overflow-hidden  overflow-y-auto overscroll-contain bg-white/90 px-8 pb-12 pt-24 font-medium transition-[opacity,visibility] duration-300 lg:visible lg:relative lg:top-0  lg:z-0 lg:flex lg:h-full lg:w-auto lg:items-stretch lg:overflow-visible lg:bg-white/0 lg:px-0 lg:py-0  lg:pt-0 lg:opacity-100 ${isToggleOpen
+                ? "visible opacity-100 backdrop-blur-sm"
+                : "invisible opacity-0"
+                }`}
+            >
+              <li role="none" className="flex items-stretch">
+                <Link
+                  href={"/"}
+                  role="menuitem"
+                  aria-haspopup="false"
+                  className={`flex items-center gap-2 py-4 transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600 focus:outline-none focus-visible:outline-none lg:px-8 ${path === "/" ? "text-emerald-500" : ""
+                    }`}
+                >
+                  <span>Home</span>
+                </Link>
+              </li>
+              <li role="none" className="flex items-stretch">
+                <Link
+                  href={"/aboutUs"}
+                  role="menuitem"
+                  aria-current="page"
+                  aria-haspopup="false"
+                  className={`flex items-center gap-2 py-4 transition-colors duration-300 hover:text-emerald-600 focus:text-emerald-600 focus:outline-none focus-visible:outline-none lg:px-8 ${path === "/aboutUs" ? "text-emerald-500" : ""
+                    }`}
+                >
+                  <span>About Us</span>
+                </Link>
+              </li>
+              <li role="none" className="flex items-stretch">
+                <Link
+                  href={"/contactUs"}
+                  role="menuitem"
+                  aria-haspopup="false"
+                  className={`flex items-center gap-2 py-4 transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600 focus:outline-none focus-visible:outline-none lg:px-8 ${path === "/contactUs" ? "text-emerald-500" : ""
+                    }`}
+                >
+                  <span>Contact Us</span>
+                </Link>
+              </li>
+            </ul>
+            <div className="ml-auto flex items-center px-6 lg:ml-0 lg:p-0">
+              {/*        <!-- Avatar --> */}
+              {/* <a
+                                href="#"
+                                className="relative inline-flex h-10 w-10 items-center justify-center rounded-full text-white"
+                            >
+                                <img
+                                    src="https://i.pravatar.cc/40?img=35"
+                                    alt="user name"
+                                    title="user name"
+                                    width="40"
+                                    height="40"
+                                    className="max-w-full rounded-full"
+                                />
+                                <span className="absolute bottom-0 right-0 inline-flex items-center justify-center gap-1 rounded-full border-2 border-white bg-pink-500 p-1 text-sm text-white">
+                                    <span className="sr-only"> 7 new emails </span>
+                                </span>
+                            </a> */}
+              <Link href={"/login"}>
+                <button className='bg-green-600 text-sm text-white font-semibold px-3 py-2 rounded-md hover:scale-105'>Login</button>
+              </Link>
+              {/*        <!-- End Avatar --> */}
+            </div>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,5 +1,5 @@
 "use client"
-
+import { TypeAnimation } from 'react-type-animation';
 import Lottie from "lottie-react";
 import bannerPic from "/src/banner.json"
 
@@ -10,7 +10,28 @@ const Banner = () => {
             <div className="bg-[url(https://i.ibb.co/fCw2ZyP/ecobin-hero1.jpg)] bg-cover lg:h-screen md:h-full h-full flex justify-center">
                 <div className="flex lg:flex-row md:flex-row flex-col-reverse justify-between items-center mx-auto lg:px-40 md:px-20 px-5 lg:gap-20 md:gap-20 pt-10 gap-8 lg:my-0 md:my-16 my-14">
                     <div className="lg:w-1/2 md:w-1/2 w-full lg:space-y-3 md:space-y-2 space-y-0 lg:text-left md:text-left text-center">
-                        <h1 className="lg:text-4xl md:text-2xl text-xl font-bold pb-5 text-[#24698C]" >Sustainable Solutions for a Cleaner Tomorrow</h1>
+                        <h1 className="lg:text-4xl md:text-2xl text-xl font-bold pb-5 text-[#24698C]" >
+                            <TypeAnimation
+                                sequence={[
+                                    // Same substring at the start will only be typed out once, initially
+                                    'Sustainable',
+                                    1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                    'Sustainable Solutions',
+                                    1000,
+                                    'Sustainable Solutions for a',
+                                    1000,
+                                    'Sustainable Solutions for a Cleaner',
+                                    1000,
+                                    'Sustainable Solutions for a Cleaner Tomorrow',
+                                    1000,
+                                ]}
+                                wrapper="span"
+                                speed={50}
+                                deletionSpeed={90}
+                                style={{ fontSize: '1em', display: 'inline-block' }}
+                                repeat={Infinity}
+                            />
+                        </h1>
                         <p className="lg:text-sm md:text-xs text-xs text-gray-600 text-justify pb-5">Explore innovative waste management practices and eco-friendly solutions that redefine the future of environmental stewardship. Join us in the journey towards a cleaner, greener planet.</p>
                         <button className='bg-green-600 text-sm text-white font-medium px-3 py-2 rounded-md hover:scale-105'>Explore Now</button>
                     </div>
