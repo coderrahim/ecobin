d25282fcd68cd94fbe8fdf3f3b016c5dfb2218e7
coderrahim <coderrahimbd@gmail.com>
2024-02-09 19:40:09 +0600

    Image Slider Added

--- src/app/(userside)/page.js
+++ src/app/(userside)/page.js
@@ -1,9 +1,11 @@
 import Banner from "@/components/Banner/Banner";
+import GallaryImages from "@/components/GallaryImages/GallaryImages";
 
 const HomePage = () => {
     return (
         <div>
             <Banner></Banner>
+            <GallaryImages></GallaryImages>
         </div>
     );
 };
